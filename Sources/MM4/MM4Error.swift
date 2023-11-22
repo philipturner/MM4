75680c434e27e62d6b97ee7b43749f5fe95b8445
Philip Turner <philipturner.AR@gmail.com>
2023-11-22 10:07:06 -0500

    Add new types of force field errors

--- Sources/MM4/MM4Error.swift
+++ Sources/MM4/MM4Error.swift
@@ -7,6 +7,18 @@
 
 import OpenMM
 
+/// Identifying information for an atom.
+public struct MM4Address {
+  /// The rigid body ID.
+  var rigidBodyIndex: Int
+  
+  /// The position within the rigid body.
+  var atomIndex: UInt32
+  
+  /// The atom's element.
+  var atomicNumber: UInt8
+}
+
 /// An error returned by the MM4 library.
 public enum MM4Error: Error {
   /// The system's energy exploded.
@@ -14,8 +26,16 @@ public enum MM4Error: Error {
   /// Includes the amount of energy drift during the time interval that failed.
   case energyDrift(Double)
   
+  // TODO: MM4ForceField should intercept parameter errors, changing the rigid
+  // body ID to the correct value.
+  
   /// The force field did not have a parameter for a group of atoms.
   ///
-  /// Includes the atomic numbers of the sequence of atoms.
-  case missingParameter([UInt8])
+  /// Includes the addresses of the atoms.
+  case missingParameter([MM4Address])
+  
+  /// The force field did not support the ring; it was too small.
+  ///
+  /// Includes the addresses of the atoms, and the size of the ring.
+  case unsupportedRing([MM4Address], Int)
 }
--- Sources/MM4/RigidBody/MM4RigidBody.swift
+++ Sources/MM4/RigidBody/MM4RigidBody.swift
@@ -139,3 +139,4 @@ public struct MM4RigidBody {
     }
   }
 }
+
