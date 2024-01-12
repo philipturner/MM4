000d265743e746b7fd794e5bc60170bd1d01451d
Philip Turner <philipturner.AR@gmail.com>
2024-01-11 19:07:26 -0500

    Remove some of the mess from debugging

--- Sources/MM4/CoreObjects/MM4System+VirtualSites.swift
+++ Sources/MM4/CoreObjects/MM4System+VirtualSites.swift
@@ -61,46 +61,13 @@ extension MM4System {
 
 extension MM4System {
   func createExceptions(force: OpenMM_CustomNonbondedForce) {
-    func addExclusion(particles: SIMD2<UInt32>) {
-      let indices = self.virtualSiteReorder(particles)
-      force.addExclusion(particles: indices)
-      
-      let atomicNumber0 = parameters.atoms.atomicNumbers[Int(particles[0])]
-      let atomicNumber1 = parameters.atoms.atomicNumbers[Int(particles[1])]
-      if atomicNumber0 == 1 {
-        var copy = indices
-        copy[0] &+= 1
-        force.addExclusion(particles: copy)
-      }
-      if atomicNumber1 == 1 {
-        var copy = indices
-        copy[1] &+= 1
-        force.addExclusion(particles: copy)
-      }
-      if atomicNumber0 == 1 && atomicNumber1 == 1 {
-        var copy = indices
-        copy &+= 1
-        force.addExclusion(particles: copy)
-      }
-    }
-    
     for bond in parameters.bonds.indices {
-      addExclusion(particles: bond)
+      let indices = self.virtualSiteReorder(bond)
+      force.addExclusion(particles: indices)
     }
     for exception in parameters.nonbondedExceptions13 {
-      addExclusion(particles: exception)
-    }
-    
-    // Stop the virtual sites from interfering with themselves.
-    for atomID in parameters.atoms.indices {
-      let atomicNumber = parameters.atoms.atomicNumbers[atomID]
-      guard atomicNumber == 1 else {
-        continue
-      }
-      
-      let reordered = Int(self.reorderedIndices[atomID])
-      let exclusion = SIMD2(reordered, reordered &+ 1)
-      force.addExclusion(particles: exclusion)
+      let indices = self.virtualSiteReorder(exception)
+      force.addExclusion(particles: indices)
     }
   }
   
