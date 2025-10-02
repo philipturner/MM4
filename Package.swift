94c2e99c584f1c4f381e2a6370136ac984f5ec6c
Philip Turner <philipturner.AR@gmail.com>
2025-10-02 12:42:58 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "MM4",
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "MM4",
       targets: ["MM4"]),
@@ -18,8 +16,6 @@ let package = Package(
     .package(url: "https://github.com/philipturner/swift-openmm", branch: "main"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "MM4",
       dependencies: [
